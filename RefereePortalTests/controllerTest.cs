a533f6671ca9fba617541a8fc491f30909eb9023
Adam882094 <78448581+Adam882094@users.noreply.github.com>
2021-12-05 14:35:30 -0500

    Added Test Class

---
+++ RefereePortalTests/controllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RefereePortalTests
+{
+    [TestClass]
+    public class controllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
